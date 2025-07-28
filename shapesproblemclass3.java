e72b4c757478bf8a85607ffa300212300e3a8a36
ACsNotThere <aaethancheung@gmail.com>
2025-07-28 13:28:54 -0400

    Add files via upload

---
+++ shapesproblemclass3.java
@@ -0,0 +1,50 @@
+public class shapesproblemclass3 {
+    /*
+     * Polygon
+     * Name of n-sided polygon (pentagon, quadrilateral, triangle, etc)
+     * Shape name
+     */
+}
+class Polygon extends shapesproblemclass3 {
+
+}
+class Hexagon extends Polygon {
+
+}
+class Pentagon extends Polygon {
+
+}
+class Quadrilateral extends Polygon {
+
+}
+class Triangle extends Polygon {
+
+}
+class Square extends Quadrilateral {
+
+}
+class Rectangle extends Quadrilateral {
+
+}
+class Rhombus extends Quadrilateral {
+
+}
+class Trapezoid extends Quadrilateral {
+
+}
+class Parallelogram extends Quadrilateral {
+
+}
+class Kite extends Quadrilateral {
+
+}
+class RightTriangle extends Triangle {
+
+}
+class AcuteTriangle extends Triangle {
+
+}
+class ObtuseTriangle extends Triangle {
+
+}
+
